9cefa2fdeafb52c1cc331ae8c8474bee804f9b74
Sarang Shelke <shelkesarang01@gmail.com>
2025-02-26 14:39:15 +0530

    Added CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from app.api.endpoints import workflows
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    # Add any other domains as needed
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # Allowed origins
+    allow_credentials=True,           # Allow cookies, authorization headers, etc.
+    allow_methods=["*"],              # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],              # Allow all headers
+)
 app.include_router(workflows.router, prefix="/workflows")
 
 @app.get("/")
